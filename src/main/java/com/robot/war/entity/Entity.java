02efe36b110bc53af52895f3fe21f7a2ebba3f55
juan miguel <Miguel.Rojas@jala.university>
2024-12-03 18:41:10 -0500

    modified Entity class

--- src/main/java/com/robot/war/entity/Entity.java
+++ src/main/java/com/robot/war/entity/Entity.java
@@ -1,7 +1,16 @@
 package com.robot.war.entity;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Entity {
-    private String Entity;
-    private int healthc;
-    private int lives;
+    private String name;
+    private int health = 100;
+    private int lives = 5;
+    private int powerLevel;
+
 }
+
+
