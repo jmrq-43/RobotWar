a77a655bcd08ffafb3423be8cbf41db05a36592f
juan miguel <Miguel.Rojas@jala.university>
2024-12-05 14:41:26 -0500

    first configuration of play class

--- src/main/java/com/robot/war/play/Play.java
+++ src/main/java/com/robot/war/play/Play.java
@@ -1,4 +1,12 @@
 package com.robot.war.play;
 
+import com.robot.war.play.controller.Controller;
+
 public class Play {
+    public static void main(String[] args) {
+        Controller controller = new Controller("cancerbero", 50,"tairon", 20);
+
+        System.out.println(controller.seeHero());
+
+    }
 }
