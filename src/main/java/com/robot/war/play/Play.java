133ebc445db846143fbbdfefa0cd5e484daeef9b
juan miguel <Miguel.Rojas@jala.university>
2024-12-05 18:55:50 -0500

    correction of Play class

--- src/main/java/com/robot/war/play/Play.java
+++ src/main/java/com/robot/war/play/Play.java
@@ -4,7 +4,7 @@ import com.robot.war.play.controller.Controller;
 
 public class Play {
     public static void main(String[] args) {
-        Controller controller = new Controller("cancerbero", 50,"tairon", 20);
+        Controller controller = new Controller("cancerbero", 50, "tairon", 20);
 
         System.out.println(controller.seeHero());
 
