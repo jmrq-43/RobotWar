cb2a6ee76b6c7ec30a52c101cd941465688d190b
juan miguel <Miguel.Rojas@jala.university>
2024-12-07 11:27:07 -0500

    add verifier class

---
+++ src/main/java/com/robot/war/play/controller/Verifier.java
@@ -0,0 +1,16 @@
+package com.robot.war.play.controller;
+
+import com.robot.war.entity.Entity;
+import com.robot.war.entity.Hero;
+import com.robot.war.entity.Villian;
+
+public class Verifier {
+    private Hero hero;
+    private Villian villian;
+
+
+    public static <T extends Entity> boolean healthVerifier(T entity){
+        return entity.getHealth() == 0;
+    }
+
+}
